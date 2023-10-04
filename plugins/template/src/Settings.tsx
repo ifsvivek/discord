5492d789dd86b93c2ee72bc74e305e77a8b49246
Vivek Sharma <80456739+ifsvivek@users.noreply.github.com>
2023-10-04 16:05:39 +0530

    first

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -1,8 +1,28 @@
-import { Forms } from "@vendetta/ui/components";
-const { FormText } = Forms;
+import { React, url } from "@vendetta/metro/common"
+import { getAssetIDByName } from "@vendetta/ui/assets"
+import { Forms, General } from "@vendetta/ui/components"
+import { showToast } from "@vendetta/ui/toasts"
 
-export default () => (
-    <FormText>
-        Hello, world!
-    </FormText>
-)
+import { fetchData } from "./index"
+
+const { ScrollView } = General
+const { FormSection, FormRow } = Forms
+
+export default () => (<ScrollView>
+    <FormSection>
+        <FormRow
+            label="Discord Server"
+            leading={<FormRow.Icon source={getAssetIDByName("Discord")} />}
+            trailing={FormRow.Arrow}
+        />
+        <FormRow
+            label="Reload DB"
+            leading={<FormRow.Icon source={getAssetIDByName("ic_message_retry")} />}
+            onPress={async () => {
+                const fetch = await fetchData()
+                if (!fetch) return showToast("Failed to reload DB", getAssetIDByName("small"))
+                return showToast("Reloaded DB", getAssetIDByName("check"))
+            }}
+        />
+    </FormSection>
+</ScrollView>)
--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,12 +1,42 @@
-import { logger } from "@vendetta";
-import Settings from "./Settings";
+import { logger } from "@vendetta"
+import { findByProps } from "@vendetta/metro"
+import { after } from "@vendetta/patcher"
+import { safeFetch } from "@vendetta/utils"
+import { showToast } from "@vendetta/ui/toasts"
 
-export default {
-    onLoad: () => {
-        logger.log("Hello world!");
-    },
-    onUnload: () => {
-        logger.log("Goodbye, world.");
-    },
-    settings: Settings,
+import Settings from "./Settings"
+
+interface userBGData {
+    _id: string
+    uid: string
+    img: string
+    orientation: string
 }
+
+const getUserBannerURL = findByProps("default", "getUserBannerURL")
+
+let data: userBGData[]
+let unpatch: () => void
+
+export const fetchData = async () => {
+    try {
+        data = await (await safeFetch("https://raw.githubusercontent.com/ifsvivek/discordpfp/main/main.json", { cache: "no-store" })).json()
+        return data
+    } catch (e) {
+        logger.error("Failed to fetch userBG data", e)
+    }
+}
+
+export const onLoad = async () => {
+    await fetchData()
+    if (!data) return showToast("Failed to load DB")
+
+    unpatch = after("getUserBannerURL", getUserBannerURL, ([user]) => {
+        const customBanner = data?.find((i: userBGData) => i.uid === user?.id)
+        if (user?.banner === undefined && customBanner) return customBanner.img
+    })
+}
+
+export const onUnload = () => unpatch?.()
+
+export const settings = Settings
