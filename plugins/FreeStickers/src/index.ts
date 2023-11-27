d845d3041293eeee663072720f642bc16385f930
ifsvivek <vs63961@gmail.com>
2023-11-27 22:53:01 +0530

    fixed

--- plugins/FreeStickers/src/index.ts
+++ plugins/FreeStickers/src/index.ts
@@ -8,14 +8,21 @@ import Settings from "./ui/Settings";
 // On 194.4+, this nitro module is frozen
 // original fix from enmity plugin: https://github.com/colin273/enmity-plugins/blob/master/Freemoji/src/index.ts#L59
 // vendetta version: https://discord.com/channels/1015931589865246730/1062531774187573308/1149552971235995698
-const nitroModule = find(m => m.default?.canUseStickersEverywhere);
+const nitroModule = find(m => m.default?.canUseAnimatedEmojis);
 nitroModule.default = { ... nitroModule.default }
 
 const messageModule = findByProps("sendMessage", "receiveMessage");
 const { getStickerById } = findByStoreName("StickersStore");
 
+// Somewhere after 204.0, Discord renamed the method
+// "canUseStickersEverywhere" -> "canUseCustomStickersEverywhere"
+// Check if old method exists, use new method name if not
+const OLD_CHECK_NAME = "canUseStickersEverywhere";
+const NEW_CHECK_NAME = "canUseCustomStickersEverywhere";
+const CHECK_NAME = nitroModule?.default[OLD_CHECK_NAME] ? OLD_CHECK_NAME : NEW_CHECK_NAME;
+
 const patches = [
-  instead("canUseStickersEverywhere", nitroModule.default, () => true),
+  instead(CHECK_NAME, nitroModule.default, () => true),
   
   instead("sendStickers", messageModule, (args, orig) => {
     const [ channelId, stickerIds, _, extra ] = args;
